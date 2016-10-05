aee149752c4c42c9ae4ec689580640bf6bac40ec
Thomas Wolfe <tewolfe2@gmail.com>
2016-10-05 15:07:33 -0700

    itinerary solved

---
+++ itineratyFinder.js
@@ -0,0 +1,40 @@
+var itineraryApp = function(){
+
+  function itineraryFinder(unorderedItinerary) {
+    let start = findStart(unorderedItinerary);
+    console.log('start:', start);
+    let orderedItin = ''; 
+    let curr = start;
+
+    while(curr) {
+      orderedItin += `${curr} -> ${unorderedItinerary[curr] || '?'}, `;
+      curr = unorderedItinerary[curr];
+    }
+    return orderedItin;
+  }
+
+  function findStart(itinerary) {
+    // need to find the element that is NOT a destination but is a start point
+    let destinations = {};
+    let startPoint = '';
+
+    Object.keys(itinerary).forEach(start => {
+      let dest = itinerary[start];
+      destinations[dest] = true;
+    })
+
+    Object.keys(itinerary).forEach(start => {
+      if(!destinations[start]) {
+        startPoint = start;
+      }
+    })
+    return startPoint;
+  }
+
+  return {itineraryFinder}
+}();
+
+
+let unorderedItinerary = {C: 'BA', BO: 'D', G: 'C', D:'G'};
+let output = itineraryApp.itineraryFinder(unorderedItinerary);
+console.log('output:', output);
