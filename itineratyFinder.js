27b65d61a42eabc62fe1195471a0824d0244d7d9
Thomas Wolfe <tewolfe2@gmail.com>
2016-10-05 15:09:11 -0700

    itinerary solved

--- itineratyFinder.js
+++ itineratyFinder.js
@@ -15,19 +15,22 @@ var itineraryApp = function(){
 
   function findStart(itinerary) {
     // need to find the element that is NOT a destination but is a start point
+
     let destinations = {};
     let startPoint = '';
 
+    // create reverse hash
     Object.keys(itinerary).forEach(start => {
       let dest = itinerary[start];
       destinations[dest] = true;
-    })
+    });
 
+    // if the item is missing from the reverse hash than that is our start
     Object.keys(itinerary).forEach(start => {
       if(!destinations[start]) {
         startPoint = start;
       }
-    })
+    });
     return startPoint;
   }
 
